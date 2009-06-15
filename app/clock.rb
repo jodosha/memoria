4af2205b9c3497c1a00fc6724e8354659f3287a1
Luca Guidi <guidi.luca@gmail.com>
2009-06-15 13:09:38 +0200

    Don't memoize servers for now

--- app/clock.rb
+++ app/clock.rb
@@ -1,25 +1,16 @@
 class Clock
-  @@mutex = Mutex.new
-
   class << self
     def run(interval = 30)
       # TODO find a proper way to execute in background without forking
       fork do
         EventMachine.run do
           EventMachine::PeriodicTimer.new(interval) do
-            servers.each do |server|
+            Server.all.each do |server|
               EventMachine.defer proc { server.write_statistics }
             end
           end
         end
       end
     end
-
-    private
-      def servers
-        @@mutex.synchronize do
-          @@servers ||= Server.all
-        end
-      end
   end
 end
