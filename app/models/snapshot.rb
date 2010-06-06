c6886e1fabe4fcb403e272829bb16a61f89bff1a
Luca Guidi <guidi.luca@gmail.com>
2010-06-06 15:45:03 +0200

    Updated snapshot informations, according to the new Redis stats

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -4,9 +4,22 @@ class Snapshot
   property   :id,                         Serial
   belongs_to :server
   property   :alive,                      Boolean
+  property   :process_id,                 Integer, :default => 0
   property   :used_memory,                Integer, :default => 0
-  property   :total_connections_received, Integer, :default => 0
+  property   :used_memory_human,          String
+  property   :vm_enabled,                 Boolean, :default => false
+  property   :arch_bits,                  String
+  property   :role,                       String
   property   :connected_clients,          Integer, :default => 0
+  property   :total_connections_received, Integer, :default => 0
+  property   :connected_slaves,           Integer, :default => 0
+  property   :bgsave_in_progress,         Boolean
+  property   :last_save_time,             Integer
+  property   :uptime_in_days,             Integer
+  property   :uptime_in_seconds,          Integer
   property   :total_commands_processed,   Integer, :default => 0
-  property   :created_at,                 DateTime
+  property   :changes_since_last_save,    Integer, :default => 0
+  property   :redis_version,              String
+  property   :multiplexing_api,           String
+  property   :created_at,                 Time
 end
